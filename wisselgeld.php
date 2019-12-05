792e386ad3330b18c1c85a46f5371a899b43d09f
Iz-Dine Amadou <2088346@talnet.nl>
2019-12-05 11:44:04 +0100

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -4,7 +4,7 @@ $Geld = Intval($invoer);
 $pos = strpos($argv[1], " ");
 
 
-if (is_numeric($invoer < 0) ) {
+if (is_numeric($invoer) && $invoer > 0 ) {
 $posint = (int)$argv[1];
 echo ("€" . $posint . " X €1 euro ");
 }
